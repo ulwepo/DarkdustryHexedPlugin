587fcf208a213ee705ccf110aa3e1e7cae5d225b
Darkness6030 <79508138+Darkness6030@users.noreply.github.com>
2021-07-29 14:42:08 +0300

    Fix server crash

--- src/hexed/HexedMod.java
+++ src/hexed/HexedMod.java
@@ -86,6 +86,7 @@ public class HexedMod extends Plugin{
                 data.updateStats();
 
                 for(Player player : Groups.player){
+                    if(player.team() == null || player == null) return;
                     if(player.team() != Team.derelict && player.team().cores().isEmpty()){
                         player.clearUnit();
                         killTiles(player.team());
