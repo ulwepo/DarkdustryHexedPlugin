021a6bba89fb69b05ed83807455b6f490b20a961
Darkness6030 <79508138+Darkness6030@users.noreply.github.com>
2021-08-24 14:14:51 +0300

    Fix

--- src/java/hexed/HexedMod.java
+++ src/java/hexed/HexedMod.java
@@ -377,8 +377,7 @@ public class HexedMod extends Plugin{
 
             for(Player player : Groups.player){
                 Call.infoMessage(player.con, Bundle.format("round-over", findLocale(player.locale)) + 
-                        (player == players.first() ? Bundle.format("you-won", findLocale(player.locale)) : "[yellow]" + players.first().name + Bundle.format("player-won", findLocale(player.locale))) +
-                        data.getControlled(players.first()).size + Bundle.format("hexes", findLocale(player.locale))
+                        (player == players.first() ? Bundle.format("you-won", findLocale(player.locale), data.getControlled(players.first()).size) : "[yellow]" + players.first().name + Bundle.format("player-won", findLocale(player.locale), data.getControlled(players.first()).size)) + 
                         + (dominated ? "" : Bundle.format("final-score", findLocale(player.locale), builder.toString())));
             }
         }
