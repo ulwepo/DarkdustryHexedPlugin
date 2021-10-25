034f70e3172bf5e61c6c9c93429866357b01fcac
SSTentacleSS <ss_tentacle_ss@zohomail.eu>
2021-10-25 09:26:28 +0300

    init

---
+++ src/main/java/org/darkdustry/Main.java
@@ -0,0 +1,26 @@
+package org.darkdustry;
+
+import arc.util.CommandHandler;
+import mindustry.mod.Plugin;
+
+public class Main extends Plugin {
+
+	public static void main(String[] args) {
+		throw new Error("This project can't be used as executable!");
+	}
+
+	@Override
+	public void init() {
+		super.init();
+	}
+
+	@Override
+	public void registerClientCommands(CommandHandler handler) {
+		super.registerClientCommands(handler);
+	}
+
+	@Override
+	public void registerServerCommands(CommandHandler handler) {
+		super.registerServerCommands(handler);
+	}
+}
