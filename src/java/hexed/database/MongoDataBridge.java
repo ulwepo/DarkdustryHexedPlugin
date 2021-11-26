c2321291f501dbab11155e06f3c1be0313a8aaf4
Darkness#3729 <79508138+Darkness6030@users.noreply.github.com>
2021-11-26 19:11:34 +0300

    Update MongoDataBridge.java

--- src/java/hexed/database/MongoDataBridge.java
+++ src/java/hexed/database/MongoDataBridge.java
@@ -1,11 +1,13 @@
 package hexed.database;
 
+
 import arc.util.Log;
 import com.mongodb.BasicDBObject;
 import com.mongodb.client.model.FindOneAndUpdateOptions;
 import com.mongodb.client.model.ReturnDocument;
 import com.mongodb.reactivestreams.client.MongoCollection;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 import org.bson.types.ObjectId;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
@@ -14,118 +16,124 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
 import java.util.function.Consumer;
 
 public abstract class MongoDataBridge<T extends MongoDataBridge<T>> {
     private static MongoCollection<Document> collection;
-    private static final Set<String> specialKeys = Set.of("_id", "__v");
+    private static final Set<String> specialKeys = Set.of(
+        "_id", "__v", "DEFAULT_CODEC_REGISTRY"
+    );
+
     public ObjectId _id;
     public int __v;
     private Map<String, Object> latest = new HashMap<>();
-
-    public MongoDataBridge() {}
 
     public static void setSourceCollection(MongoCollection<Document> collection) {
         MongoDataBridge.collection = collection;
     }
 
-    public static MongoCollection<Document> getSourceCollection() {
-        return collection;
-    }
-
-    public void save(final Consumer<Throwable> callback) {
-        Map<String, Object> values = this.getDeclaredPublicFields();
-        BasicDBObject operations = this.toBsonOperations(this.latest, values);
-        if (!operations.isEmpty()) {
-            this.latest = values;
-            collection.findOneAndUpdate(new BasicDBObject("_id", values.get("_id")), operations, (new FindOneAndUpdateOptions()).upsert(true).returnDocument(ReturnDocument.AFTER)).subscribe(new Subscriber<>() {
-                public void onSubscribe(Subscription s) {
-                    s.request(1L);
-                }
-
-                public void onNext(Document t) {}
-
-                public void onComplete() {
-                    callback.accept(null);
-                }
-
-                public void onError(Throwable t) {
-                    if (!Objects.isNull(t)) {
-                        callback.accept(t);
-                    }
-                }
-            });
-        }
-    }
-
     public void save() {
-        save(e -> {
-            if (e != null) Log.err(e);
+        Map<String, Object> values = getDeclaredPublicFields();
+
+        BasicDBObject operations = toBsonOperations(latest, values);
+        
+        if (!operations.isEmpty()) latest = values;
+        collection.findOneAndUpdate(
+            new BasicDBObject("_id", values.get("_id")),
+            operations,
+            new FindOneAndUpdateOptions().upsert(true).returnDocument(ReturnDocument.AFTER)
+        ).subscribe(new Subscriber<>() {
+            @Override
+            public void onSubscribe(Subscription s) {
+                s.request(1);
+            }
+
+            @Override
+            public void onNext(Document t) {}
+
+            @Override
+            public void onComplete() {}
+
+            @Override
+            public void onError(Throwable t) {
+                Log.err(t);
+            }
         });
     }
 
-    public void resetLatest() {
-        this.latest = this.getDeclaredPublicFields();
-    }
-
-    public static <T extends MongoDataBridge<T>> void find(Class<T> clazz, BasicDBObject filter, final Consumer<T> callback) {
+    public static <T extends MongoDataBridge<T>>void findAndApplySchema(Class<T> sourceClass, Bson filter, Consumer<T> callback) {
         try {
-            final T dataClass = clazz.getConstructor().newInstance();
-            final Set<Field> fields = Set.of(clazz.getFields());
+            T dataClass = sourceClass.getConstructor().newInstance();
+
+            Set<Field> fields = Set.of(sourceClass.getFields());
             Document defaultObject = new Document();
+
             fields.forEach(field -> {
                 if (!specialKeys.contains(field.getName())) {
                     try {
                         defaultObject.append(field.getName(), field.get(dataClass));
-                    } catch (IllegalAccessException | IllegalArgumentException e) {
+                    } catch (IllegalArgumentException | IllegalAccessException e) {
                         Log.err(e);
                     }
                 }
             });
-            filter.forEach(defaultObject::append);
-            collection.findOneAndUpdate(filter, new BasicDBObject("$setOnInsert", defaultObject), (new FindOneAndUpdateOptions()).upsert(true).returnDocument(ReturnDocument.AFTER)).subscribe(new Subscriber<>() {
+
+            filter.toBsonDocument().forEach(defaultObject::append);
+
+            collection.findOneAndUpdate(filter,
+                    new BasicDBObject("$setOnInsert", defaultObject),
+                    new FindOneAndUpdateOptions().upsert(true).returnDocument(ReturnDocument.AFTER)
+            ).subscribe(new Subscriber<>() {
+                @Override
                 public void onSubscribe(Subscription s) {
-                    s.request(1L);
+                    s.request(1);
                 }
 
+                @Override
                 public void onNext(Document document) {
                     fields.forEach(field -> {
                         try {
                             field.set(dataClass, document.getOrDefault(field.getName(), field.get(dataClass)));
-                        } catch (IllegalAccessException | IllegalArgumentException e) {
-                            Log.err(e);
+                        } catch (IllegalArgumentException | IllegalAccessException e) {
+                            e.printStackTrace();
                         }
-
                     });
+
                     dataClass.resetLatest();
                     callback.accept(dataClass);
                 }
 
+                @Override
                 public void onComplete() {}
 
-                public void onError(Throwable t) {
-                    if (!Objects.isNull(t)) {
-                        Log.err(t);
-                    }
-                }
+                @Override
+                public void onError(Throwable t) {}
             });
         } catch (Exception e) {
             Log.err(e);
         }
     }
 
+    public void resetLatest() {
+        this.latest = getDeclaredPublicFields();
+    }
+
+    @Override
+    public String toString() {
+        return getDeclaredPublicFields().toString();
+    }
+
     private Map<String, Object> getDeclaredPublicFields() {
         Field[] fields = this.getClass().getFields();
+        
         Map<String, Object> values = new HashMap<>();
 
         for (Field field : fields) {
-            if (Modifier.isPublic(field.getModifiers())) {
-                try {
-                    values.put(field.getName(), field.get(this));
-                } catch (Exception ignored) {}
-            }
+            if (!Modifier.isPublic(field.getModifiers())) continue;
+            try {
+                values.put(field.getName(), field.get(this));
+            } catch (Exception ignored) {}
         }
 
         return values;
@@ -134,15 +142,14 @@ public abstract class MongoDataBridge<T extends MongoDataBridge<T>> {
     private BasicDBObject toBsonOperations(Map<String, Object> previousFields, Map<String, Object> newFields) {
         Map<String, DataChanges> changes = DataChanges.getChanges(previousFields, newFields);
         Map<String, BasicDBObject> operations = new HashMap<>();
-        changes.forEach((key, changedValues) -> {
-            if (!changedValues.current.equals(DataChanges.undefined) && !specialKeys.contains(key)) {
-                if (!operations.containsKey("$set")) {
-                    operations.put("$set", new BasicDBObject());
-                }
 
-                operations.get("$set").append(key, changedValues.current);
-            }
+        changes.forEach((key, changedValues) -> {
+            if (changedValues.current.equals(DataChanges.undefined) || specialKeys.contains(key)) return;
+
+            if (!operations.containsKey("$set")) operations.put("$set", new BasicDBObject());
+            operations.get("$set").append(key, changedValues.current);
         });
+
         return new BasicDBObject(operations);
     }
 }
