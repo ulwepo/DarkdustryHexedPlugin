169bab475b58436a4e20deeb10065e261ea2d20a
xzxADIxzx <valerazhuk20122006@gmail.com>
2022-09-07 20:37:04 +0300

    почему это отдельным файлом?

--- src/java/hexed/comp/Statistics.java
+++ src/java/hexed/comp/Statistics.java
@@ -3,6 +3,9 @@ package hexed.comp;
 import arc.files.Fi;
 import arc.struct.ObjectMap;
 import arc.struct.Seq;
+import arc.util.serialization.Json;
+import arc.util.serialization.JsonValue;
+import arc.util.serialization.Json.JsonSerializable;
 import mindustry.io.JsonIO;
 
 import static mindustry.Vars.*;
@@ -31,5 +34,21 @@ public class Statistics {
         return datas.values().toSeq()
                 .filter(data -> data.wins > 0)
                 .sort(data -> -data.wins);
+    }
+
+    public static class PlayerData implements JsonSerializable {
+
+        public String name;
+        public int wins;
+
+        public void write(Json json) {
+            json.writeValue("name", name);
+            json.writeValue("wins", wins);
+        }
+
+        public void read(Json json, JsonValue data) {
+            name = data.getString("name");
+            wins = data.getInt("wins");
+        }
     }
 }
