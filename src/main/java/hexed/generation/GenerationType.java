e5ecd68e7fc2d0e4e5e74df1450eeb99589c59a4
Даркнесс <inflexibled@gmail.com>
2022-09-17 16:38:40 +0300

    дорезал фильтры

--- src/main/java/hexed/generation/GenerationType.java
+++ src/main/java/hexed/generation/GenerationType.java
@@ -14,18 +14,12 @@ public class GenerationType {
     public final String name;
     public final Planet planet;
 
-    public final Block defaultFloor;
-    public final Block defaultBlock;
-
     public final Cons<Rules> ruleSetter;
     public final Seq<GenerateFilter> filters = new Seq<>();
 
-    public GenerationType(String name, Planet planet, Block floor, Block block, Cons<Rules> ruleSetter, GenerateFilter... filters) {
+    public GenerationType(String name, Planet planet, Cons<Rules> ruleSetter, GenerateFilter... filters) {
         this.name = name;
         this.planet = planet;
-
-        this.defaultFloor = floor;
-        this.defaultBlock = block;
 
         this.ruleSetter = ruleSetter;
         this.filters.addAll(filters);
--- src/main/java/hexed/generation/GenerationTypes.java
+++ src/main/java/hexed/generation/GenerationTypes.java
@@ -2,117 +2,95 @@ package hexed.generation;
 
 import arc.graphics.Color;
 import arc.struct.Seq;
-import mindustry.content.Blocks;
-import mindustry.content.Planets;
-import mindustry.content.Weathers;
+import mindustry.content.*;
 import mindustry.maps.filters.*;
 import mindustry.type.Weather.WeatherEntry;
 
-import static hexed.Main.*;
+import static hexed.Main.type;
 
 public class GenerationTypes {
 
     public static GenerationType beta;
 
     public static void load() {
-        beta = new GenerationType("[white]\uF7EA [accent]Beta", Planets.serpulo, Blocks.moss, Blocks.sporeWall, rules -> {
+        beta = new GenerationType("[white]\uF7EA [accent]Beta", Planets.serpulo, rules -> {
             rules.lighting = true;
             rules.ambientLight = Color.black.cpy().a(.7f);
 
             rules.weather.add(new WeatherEntry(Weathers.sporestorm, 5f, 10f, 1f, 2f) {{
                 intensity = .25f;
             }});
-        },
-                new NoiseFilter() {{
-                    scl = 39.92f;
-                    octaves = 3.015f;
-                    floor = Blocks.rhyolite;
-                    block = Blocks.rhyoliteWall;
-                }},
-
-                new NoiseFilter() {{
-                    scl = 39.92f;
-                    octaves = 3.015f;
-                    floor = Blocks.regolith;
-                    block = Blocks.regolithWall;
-                }},
-
-                new NoiseFilter() {{
-                    scl = 39.92f;
-                    octaves = 3.015f;
-                    floor = Blocks.arkyicStone;
-                    block = Blocks.arkyicWall;
-                }},
-
-                new NoiseFilter() {{
-                    scl = 498.99997f;
-                    threshold = 0f;
-                    octaves = 1f;
-                    falloff = 0f;
-                    tilt = -4f;
-                    floor = Blocks.carbonStone;
-                    block = Blocks.carbonWall;
-                    target = Blocks.stone;
-                }},
-
-                new OreFilter() {{
-                    scl = 24.949999f;
-                    threshold = 0.84499997f;
-                    octaves = 1.98f;
-                    falloff = 0.29999998f;
-                    ore = Blocks.wallOreTungsten;
-                }},
-
-                new OreFilter() {{
-                    scl = 24.949999f;
-                    threshold = 0.88f;
-                    octaves = 1.98f;
-                    falloff = 0.29999998f;
-                    ore = Blocks.wallOreThorium;
-                }},
-
-                new OreFilter() {{
-                    scl = 34.93f;
-                    octaves = 1.98f;
-                    falloff = 0.29999998f;
-                    ore = Blocks.wallOreBeryllium;
-                }},
-
-                new NoiseFilter() {{
-                    scl = 79.84f;
-                    threshold = 0.72499996f;
-                    octaves = 3.015f;
-                    floor = Blocks.slag;
-                    target = Blocks.carbonStone;
-                }},
-
-                new NoiseFilter() {{
-                    scl = 29.939999f;
-                    threshold = 0.695f;
-                    octaves = 3.015f;
-                    falloff = 0.69f;
-                    floor = Blocks.arkyciteFloor;
-                    target = Blocks.arkyicStone;
-                }},
-
-                new ScatterFilter() {{
-                    chance = 0.5f;
-                    flooronto = Blocks.arkyicStone;
-                    floor = Blocks.arkyicVent;
-                }},
-
-                new ScatterFilter() {{
-                    chance = 0.5f;
-                    flooronto = Blocks.carbonStone;
-                    floor = Blocks.carbonVent;
-                }},
-
-                new ScatterFilter() {{
-                    chance = 0.5f;
-                    flooronto = Blocks.rhyolite;
-                    floor = Blocks.rhyoliteVent;
-                }}
-        );
+        }, new NoiseFilter() {{
+            scl = 39.92f;
+            octaves = 3.015f;
+            floor = Blocks.rhyolite;
+            block = Blocks.rhyoliteWall;
+        }}, new NoiseFilter() {{
+            scl = 39.92f;
+            octaves = 3.015f;
+            floor = Blocks.regolith;
+            block = Blocks.regolithWall;
+        }}, new NoiseFilter() {{
+            scl = 39.92f;
+            octaves = 3.015f;
+            floor = Blocks.arkyicStone;
+            block = Blocks.arkyicWall;
+        }}, new NoiseFilter() {{
+            scl = 498.99997f;
+            threshold = 0f;
+            octaves = 1f;
+            falloff = 0f;
+            tilt = -4f;
+            floor = Blocks.carbonStone;
+            block = Blocks.carbonWall;
+            target = Blocks.stone;
+        }}, new OreFilter() {{
+            scl = 30f;
+            threshold = 0.78f;
+            octaves = 1.98f;
+            falloff = 0.29999998f;
+            ore = Blocks.wallOreTungsten;
+        }}, new OreFilter() {{
+            scl = 30f;
+            octaves = 1.98f;
+            falloff = 0.29999998f;
+            ore = Blocks.wallOreThorium;
+        }}, new OreFilter() {{
+            scl = 38f;
+            octaves = 1.98f;
+            falloff = 0.29999998f;
+            ore = Blocks.wallOreBeryllium;
+        }}, new NoiseFilter() {{
+            scl = 79.84f;
+            threshold = 0.695f;
+            octaves = 3.015f;
+            floor = Blocks.slag;
+            target = Blocks.carbonStone;
+        }}, new NoiseFilter() {{
+            scl = 29.939999f;
+            threshold = 0.695f;
+            octaves = 3.015f;
+            falloff = 0.69f;
+            floor = Blocks.arkyciteFloor;
+            target = Blocks.arkyicStone;
+        }}, new NoiseFilter() {{
+            scl = 2.5f;
+            floor = Blocks.carbonStone;
+            block = Blocks.graphiticWall;
+            target = Blocks.carbonWall;
+        }}, new ScatterFilter() {{
+            chance = 0.5f;
+            flooronto = Blocks.arkyicStone;
+            floor = Blocks.arkyicVent;
+        }}, new ScatterFilter() {{
+            chance = 0.5f;
+            flooronto = Blocks.carbonStone;
+            floor = Blocks.carbonVent;
+        }}, new ScatterFilter() {{
+            chance = 0.5f;
+            flooronto = Blocks.rhyolite;
+            floor = Blocks.rhyoliteVent;
+        }});
     }
 
     public static Seq<GenerationType> all() {
