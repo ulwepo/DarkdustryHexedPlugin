2557bb49a98522b2a40efe22f4322941abee0b2e
Даркнесс#3729 <79508138+Darkness6030@users.noreply.github.com>
2022-09-28 11:47:44 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ dependencies {
     project.version = json["version"]!!
 
     compileOnly("com.github.Anuken.Arc:arc-core:v$mindustryVersion")
-    compileOnly("com.github.Anuken.Mindustry:core:138")
+    compileOnly("com.github.Anuken.Mindustry:core:v138")
 }
 
 tasks.jar {
